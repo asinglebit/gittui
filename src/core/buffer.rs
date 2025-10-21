53f62df88fe23580aca2c79315146a8763b47bdd
rattleworks <gasimov.abdulali@gmail.com>
2025-10-21 17:43:19 +0200

    Checkpoint

--- src/core/buffer.rs
+++ src/core/buffer.rs
@@ -133,7 +133,7 @@ impl Buffer {
         let old = std::mem::take(&mut self.delta);
         self.deltas.push_back(old);
         let idx = self.deltas.len().saturating_sub(1);
-        if  idx % 500 == 0 {
+        if  idx % 100 == 0 {
             self.checkpoints.insert(idx, self.curr.clone());
         }        
     }
